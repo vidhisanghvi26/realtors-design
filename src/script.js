9c87d37a0303d10c67cbbcbb504aa8b59f0a6325
Vidhi Sanghvi <vidhisanghvi@Vidhis-MacBook-Pro.local>
2025-02-23 20:54:15 -0800

    home page v2

--- src/script.js
+++ src/script.js
@@ -10,27 +10,14 @@ document.addEventListener('DOMContentLoaded', () => {
     if (currentNavLinkId) {
         document.getElementById(currentNavLinkId).classList.add('selected');
     }
-    console.log('currentPath', currentPath);
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-    const currentPath = window.location.pathname;
-    const navLinks = {
-        '/index.html': 'nav-home',
-        '/about-us.html': 'nav-about',
-        '/listings.html': 'nav-listings'
-    };
-
-    const currentNavLinkId = navLinks[currentPath];
-    if (currentNavLinkId) {
-        document.getElementById(currentNavLinkId).classList.add('selected');
-    }
-});
 
 function handleContactButtonClick() {
     alert('Contact Us button clicked!');
 }
 
 function handleExploreButtonClick() {
-    alert('Explore Listing button clicked!');
+    console.log('Explore Listing button clicked!');
+    window.location.href = 'listings.html';
 }
